928b4f4496146328fddc90218b56bf315009f131
atihar <mahir_1990@yahoo.com>
2021-08-09 22:35:49 +0600

    project page updated

--- resources/views/projects.blade.php
+++ resources/views/projects.blade.php
@@ -5,7 +5,7 @@
             <div class="container pt-lg-8 pb-lg-6 pt-md-7 pb-md-5 pt-5 pb-4">
                 <div class="row">
                     <div class="col-lg-6">
-                        <h2 class="my-3">Portfolio</h2>
+                        <h2 class="my-3">Mentionable projects </h2>
                         <p class="lead mb-3">Some of the notable projects that we have done for our clients which is playing a vital role for the national growth and infrastructural development.</p>
                     </div>
                 </div>
@@ -14,49 +14,124 @@
         <!-- portfoio-->
         <div class="bg-twenty">
             <div class="container mb-lg-9">
-                <div class="portfolio-list">
-                    <article class="portfolio-item">
-                        <div class="entry-media"><img src="assets/images/3.jpg" alt="project"></div>
-                        <div class="entry-content-wrapper">
-                            <h2 class="entry-title"><a class="text-white" href="#">BD ecrrp school cyclone shelter</a></h2>
-                        </div>
-                    </article>
-                    <article class="portfolio-item">
-                        <div class="entry-media"><img src="assets/images/4.jpg" alt="project"></div>
-                        <div class="entry-content-wrapper">
-                            <h2 class="entry-title"><a class="text-white" href="#">Cumilla-Noakhali Four Lane Road</a></h2>
-                        </div>
-                    </article>
-                    <article class="portfolio-item">
-                        <div class="entry-media"><img src="assets/images/5.jpg" alt="project"></div>
-                        <div class="entry-content-wrapper">
-                            <h2 class="entry-title"><a class="text-white" href="#">Noakhali Government College Building</a></h2>
-                        </div>
-                    </article>
-                    <article class="portfolio-item">
-                        <div class="entry-media"><img src="assets/images/28.jpg" alt="project"></div>
-                        <div class="entry-content-wrapper">
-                            <h2 class="entry-title"><a class="text-white" href="#">Multipurpose Architecture and Construction</a></h2>
-                        </div>
-                    </article>
-                    <article class="portfolio-item">
-                        <div class="entry-media"><img src="assets/images/66.jpg" alt="project"></div>
-                        <div class="entry-content-wrapper">
-                            <h2 class="entry-title"><a class="text-white" href="#">Payra Sea Port, Patuakhali </a></h2>
-                        </div>
-                    </article>
-                    <article class="portfolio-item">
-                        <div class="entry-media"><img src="assets/images/67.jpg" alt="project"></div>
-                        <div class="entry-content-wrapper">
-                            <h2 class="entry-title"><a class="text-white" href="#">Mirsarai road construction, Chittagong</a></h2>
-                        </div>
-                    </article>
-                    <article class="portfolio-item">
-                        <div class="entry-media"><img src="assets/images/68.jpg" alt="project"></div>
-                        <div class="entry-content-wrapper">
-                            <h2 class="entry-title"><a class="text-white" href="portfolio-single-project.html">JMC campus construction</a></h2>
-                        </div>
-                    </article>
+{{--                <div class="portfolio-list">--}}
+{{--                    <article class="portfolio-item">--}}
+{{--                        <div class="entry-media"><img src="assets/images/3.jpg" alt="project"></div>--}}
+{{--                        <div class="entry-content-wrapper">--}}
+{{--                            <h2 class="entry-title"><a class="text-white" href="#">BD ecrrp school cyclone shelter</a></h2>--}}
+{{--                        </div>--}}
+{{--                    </article>--}}
+{{--                    <article class="portfolio-item">--}}
+{{--                        <div class="entry-media"><img src="assets/images/4.jpg" alt="project"></div>--}}
+{{--                        <div class="entry-content-wrapper">--}}
+{{--                            <h2 class="entry-title"><a class="text-white" href="#">Cumilla-Noakhali Four Lane Road</a></h2>--}}
+{{--                        </div>--}}
+{{--                    </article>--}}
+{{--                    <article class="portfolio-item">--}}
+{{--                        <div class="entry-media"><img src="assets/images/5.jpg" alt="project"></div>--}}
+{{--                        <div class="entry-content-wrapper">--}}
+{{--                            <h2 class="entry-title"><a class="text-white" href="#">Noakhali Government College Building</a></h2>--}}
+{{--                        </div>--}}
+{{--                    </article>--}}
+{{--                    <article class="portfolio-item">--}}
+{{--                        <div class="entry-media"><img src="assets/images/28.jpg" alt="project"></div>--}}
+{{--                        <div class="entry-content-wrapper">--}}
+{{--                            <h2 class="entry-title"><a class="text-white" href="#">Multipurpose Architecture and Construction</a></h2>--}}
+{{--                        </div>--}}
+{{--                    </article>--}}
+{{--                    <article class="portfolio-item">--}}
+{{--                        <div class="entry-media"><img src="assets/images/66.jpg" alt="project"></div>--}}
+{{--                        <div class="entry-content-wrapper">--}}
+{{--                            <h2 class="entry-title"><a class="text-white" href="#">Payra Sea Port, Patuakhali </a></h2>--}}
+{{--                        </div>--}}
+{{--                    </article>--}}
+{{--                    <article class="portfolio-item">--}}
+{{--                        <div class="entry-media"><img src="assets/images/67.jpg" alt="project"></div>--}}
+{{--                        <div class="entry-content-wrapper">--}}
+{{--                            <h2 class="entry-title"><a class="text-white" href="#">Mirsarai road construction, Chittagong</a></h2>--}}
+{{--                        </div>--}}
+{{--                    </article>--}}
+{{--                    <article class="portfolio-item">--}}
+{{--                        <div class="entry-media"><img src="assets/images/68.jpg" alt="project"></div>--}}
+{{--                        <div class="entry-content-wrapper">--}}
+{{--                            <h2 class="entry-title"><a class="text-white" href="portfolio-single-project.html">JMC campus construction</a></h2>--}}
+{{--                        </div>--}}
+{{--                    </article>--}}
+{{--                </div>--}}
+                <div class="row">
+                    <div class="col-6">
+                        <img src="assets/images/5.jpg" alt="project">
+                        <p class="lead">Project: Vertical Extension of 10 storied academic Building of Noakhali Science &amp; Technology
+                            University <br>
+                            Client: Noakhali Science &amp; Technology University <br>
+                            Year: 2019</p>
+                    </div>
+                    <div class="col-6">
+                        <img src="assets/images/87.jpg" alt="project">
+                        <p class="lead">
+                            Project: Maintenance of (a) Bangla bazar- Al-Amin Bazar- Halim Bazar- Subarnachar Upazila
+                            HQ Road, <br>(b) Upazila HQ-Halim Bazar road-Al Amin Bazar-Bangla Bazar Road <br>(c) Char
+                            Amamullah UP Office- KhaserhatBanglabazar-Karimbazar Road <br>(d) RHDKhaserhat-Chomir
+                            hat road UpazilaSubarnachar, Noakhali <br>
+                            Client: Office of the Executive Engineer, LGED, Noakhali <br>
+                            Year: 2019
+                        </p>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-6">
+                        <img src="assets/images/4.jpg" alt="project">
+                        <p class="lead">Project: Maintenance of Sonapur-ThanarhatAkhter Miar Hat Road, Noakhali Sadar, District:
+                            Noakhali under Second Rural Transport Improvement Project (RTIP-II) <br>
+                            Client: Office of the Executive Engineer, LGED, Noakhali <br>
+                            Year: 2019</p>
+                    </div>
+                    <div class="col-6">
+                        <img src="assets/images/28.jpg" alt="project">
+                        <p class="lead">
+                            Project: Construction and infrastructural development: works of various educational
+                            institute under the Office of the Executive Engineer. <br>
+                            Client: Education Engineering Department, Noakhali Zone Office of the Executive Engineer,
+                            Education Engineering Department, Noakhali Zone <br>
+                            Year: 2016
+                        </p>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-6">
+                        <img src="assets/images/88.jpg" alt="project">
+                        <p class="lead">Project: Maintenance of (a) Bangla bazar-Al-Amin Bazar-Halim Bazar-Subarnachar Upazila
+                            HQ road, <br>(b) Upazila HQ-Halim Bazar-Al Amin Bazar- Bangla Bazar road, <br>(e) Char Amamullah
+                            UP Office-KhaserhatBanglabazar-Khaserhat-Chomir hal road, Upazila- Subarnachar, Noakhali <br>
+                            Client: Office of the Executive Engineer, LGED, Noakhali <br>
+                            Year: 2019</p>
+                    </div>
+                    <div class="col-6">
+                        <img src="assets/images/67.jpg" alt="project">
+                        <p class="lead">
+                            Project: Maintenance of Sonapur-ThanarhatAkhter Miar Hat road, Noakhali Sadar, District:
+                            Noakhali under Second Rural Transport Improvement Project (RTIO-II) <br>
+                            Client: Office of the Executive Engineer, LGED, Noakhali <br>
+                            Year: 2019
+                        </p>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-6">
+                        <img src="assets/images/3.jpg" alt="project">
+                        <p class="lead">Project: Improvement of Karamullah-UndercharFazumiarhat-Noakhali Sadar, Noakhali. <br>
+                            Client: Office of the Executive Engineer, LGED, Noakhali <br>
+                            Year: 2019</p>
+                    </div>
+                    <div class="col-6">
+                        <img src="assets/images/89.jpg" alt="project">
+                        <p class="lead">
+                            Project: Supply of MSR equipment to Noakhali 250 Bed General Hospital under Ministry of
+                            Health and Family Welfare <br>
+                            Client: Noakhali 250 Bed General Hospital, Ministry of Health and Family Welfare <br>
+                            Year: 2019
+                        </p>
+                    </div>
                 </div>
             </div>
         </div>
